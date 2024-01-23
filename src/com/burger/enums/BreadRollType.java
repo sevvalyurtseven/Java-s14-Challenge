acb314aea60a13f374182facb9b4a94387766bb5
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-23 23:51:30 +0300

    instance variablelar olusturuldu

---
+++ src/com/burger/enums/BreadRollType.java
@@ -0,0 +1,8 @@
+package com.burger.enums;
+
+public enum BreadRollType {
+    NORMAL,
+    WRAP,
+    SANDWICH,
+    DOUBLE_BURGER
+}
---
+++ src/com/burger/model/Hamburger.java
@@ -0,0 +1,10 @@
+package com.burger.model;
+
+import com.burger.enums.BreadRollType;
+
+public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private BreadRollType breadRollType;
+}
