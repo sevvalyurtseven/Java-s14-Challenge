2986865ac358e5d80bd6e93de065984812231a76
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-24 00:02:10 +0300

    constructor olusturuldu

--- src/com/burger/model/Hamburger.java
+++ src/com/burger/model/Hamburger.java
@@ -7,5 +7,13 @@ public class Hamburger {
     private String meat;
     private double price;
     private BreadRollType breadRollType;
-    private Addition[] additions;
+    private Addition[] additions; //dinamik hale geldi
+
+    public Hamburger(String name, String meat, double price, BreadRollType breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+        this.additions = new Addition[4];
+    }
 }
