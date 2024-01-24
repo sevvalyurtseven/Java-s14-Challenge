5e81f8c271ab89c971bd0b6a4c0cc84209a277d6
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-25 00:22:36 +0300

    addable classindaki addAddition methodu override edildi

--- src/com/burger/model/HealthyBurger.java
+++ src/com/burger/model/HealthyBurger.java
@@ -4,10 +4,27 @@ import com.burger.enums.BreadRollType;
 
 public class HealthyBurger extends Hamburger{
     private Addition healthyAddition1;
-    private Addition getHealthyAddition2;
+    private Addition healthyAddition2;
 
     public HealthyBurger(String name, double price, BreadRollType breadRollType) {
         super(name, "Tofu", price, breadRollType);
     }
 
+    public Addition getHealthyAddition1() {
+        return healthyAddition1;
+    }
+
+    public Addition getGetHealthyAddition2() {
+        return healthyAddition2;
+    }
+
+    @Override
+    public void addAddition(String name, double price) {
+        if(healthyAddition1 == null){
+            healthyAddition1 = new Addition(name, price);
+        }
+        else if (healthyAddition2 == null){
+            healthyAddition2 = new Addition(name, price);
+        }
+    }
 }
